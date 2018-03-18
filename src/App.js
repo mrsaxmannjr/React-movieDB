259191de3a81450e8f8d42a113313a216bbcb111
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-03-17 20:21:19 -0600

    added redux

--- src/App.js
+++ src/App.js
@@ -5,26 +5,41 @@ import {
   Switch,
   Link,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import styled from "styled-components";
 import "./App.css";
 
+import rootReducer from "./rootReducer";
+
 import MoviesList from "./MoviesList";
 import MovieDetail from "./MovieDetail";
+import Toggle from "./Toggle";
+
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(),
+);
 
 const App = () => (
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/">
-          <HeaderH1>SM-MDB</HeaderH1>
-        </Link>
-      </header>
-      <Switch>
-        <Route exact path="/" component={MoviesList} />
-        <Route path="/:id" component={MovieDetail} />
-      </Switch>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">
+            <HeaderH1>SM-MDB</HeaderH1>
+          </Link>
+        </header>
+        <Toggle />
+        <Switch>
+          <Route exact path="/" component={MoviesList} />
+          <Route path="/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
 );
 export default App;
 
---
+++ src/Toggle.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import { toggleMessage } from "./actions";
+
+const Toggle = ({ messageVisibility, toggleMessage }) => (
+  <div>
+    {messageVisibility &&
+    <p>You will be seeing this is redux action is toggeld</p>
+    }
+    <button onClick={toggleMessage}>Toggle Me
+    </button>
+  </div>
+);
+
+
+const mapStateToProps = state => ({
+  messageVisibility: state.message.messageVisibility,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  toggleMessage,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Toggle);
